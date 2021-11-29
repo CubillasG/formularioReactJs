600e4662de76b5f0b24719188ae8110362f917c4
Gonzalo Cubillas <gonzalo.cubillas15@gmail.com>
2021-11-29 19:33:51 -0300

    agregando checkbox

--- src/Componente/Formulario.jsx
+++ src/Componente/Formulario.jsx
@@ -1,49 +1,53 @@
-import { useState} from 'react'
+import { useState } from "react"
+
+
 
 const Formulario = () => {
-    const [todo, setTodo] = useState({
-        todoNombre:'',
-        todoDescripcion:'',
-        todoSelection:'',
-    });
 
-    const handlechange = (e) =>{
-        console.log(e.target.value);
-        console.log(e.target.name);
-        
-        setTodo({
-            ...todo,
-            [e.target.name] : e.target.value
-        })
-        
-    }
-    
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-       console.log(todo)
+   const [todo, setTodo] = useState({
+       todoNombre:'',
+       todoDescripcion:'',
+       todoSelection:'',
+       todoCheck: false,
+   }) 
 
-    }
+
+
+   const handleSubmit = (e) =>{
+       e.target.prevenDefault()
+       
+      
+   }
+
+   const handleChange = (e)=>{
+    const {name,type,checked,value} = e.target
+      
+    setTodo({
+           
+           ...todo,
+           [name]: type === 'checkbox' ? checked : value
+
+       })
+   }
+
    
-
-   
-    
-    return (
+return (
         <>
           <form onSubmit={handleSubmit}>
               <input 
               name='todoNombre'
               className='form-control mb-2'
               placeholder='Ingrese su name'
-              onChange={handlechange}
+              onChange={handleChange}
               value={todo.todoNombre}
-             
+              
               />
 
               <textarea 
               name='todoDescripcion'
               className='form-control mb-2'
               placeholder='Descripcion'
-              onChange={handlechange}
+              onChange={handleChange}
               value={todo.todoDescripcion}
               >
               </textarea>
@@ -51,17 +55,41 @@ const Formulario = () => {
               <select 
               name="todoSelection" 
               className='form-control mb-2'
-              onChange={handlechange}
+              onChange={handleChange}
               value={todo.todoSelection}
+            
               >
                   <option value="pendiente">Pendiente</option>
                   <option value="completo">Completado</option>
               </select>
 
+              <div className="form-check">
+                     <input 
+                     className="form-check-input" 
+                     type="checkbox"
+                     name='todoCheck' 
+                     checked={todo.todoCheck}
+                     onChange={handleChange}
+                 
+                     
+                     
+                     
+                     />
+                     
+                     <label 
+                     className="form-check-label" 
+                     htmlFor="flexCheckDefault"
+                     >Dar prioridad
+                     </label>
+                     
+                     
+             </div>
+
              
 
               <button 
               className='btn btn-primary mb-2'
+              type='submit'
               
               >
                   Enviar
